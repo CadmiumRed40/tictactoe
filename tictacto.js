defbeb0d8ce321568d85deed62e41c182b6be854
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-04-05 21:44:26 -0400

    creating more game variables

--- tictacto.js
+++ tictacto.js
@@ -1,3 +1,30 @@
+//array of the game board
+const gameBoard =
+    [1,2,3,
+     4,5,6,
+     7,8,9]
+
+//solutions
+const solutions = 
+    [1,2,3]
+    [1,5,9]
+    [1,4,7]
+    [3,6,9]
+    [2,5,8]
+    [3,5,7]
+    [4,5,6]
+    [7,8,9]
+
+const one = document.getElementsByClassName('1').addEventListener('click')
+const two = document.getElementsByClassName('2').addEventListener('click')
+const three = document.getElementsByClassName('3').addEventListener('click')
+const four = document.getElementsByClassName('4').addEventListener('click')
+const five = document.getElementsByClassName('5').addEventListener('click')
+const six = document.getElementsByClassName('6').addEventListener('click')
+const seven = document.getElementsByClassName('7').addEventListener('click')
+const eight = document.getElementsByClassName('8').addEventListener('click')
+const nine = document.getElementsByClassName('9').addEventListener('click')
+
 // function for choosing one player game
 function onePlayerGame(){
     document.getElementById('player1Button').addEventListener('click', chosen)
