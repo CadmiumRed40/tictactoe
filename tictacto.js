f2d1b88413b85b0a91c54b72b2773f2ba641864e
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-04-06 18:37:40 -0400

    idk

--- tictacto.js
+++ tictacto.js
@@ -1,12 +1,13 @@
-const one = document.getElementsByClassName('1').addEventListener('click')
-const two = document.getElementsByClassName('2').addEventListener('click')
-const three = document.getElementsByClassName('3').addEventListener('click')
-const four = document.getElementsByClassName('4').addEventListener('click')
-const five = document.getElementsByClassName('5').addEventListener('click')
-const six = document.getElementsByClassName('6').addEventListener('click')
-const seven = document.getElementsByClassName('7').addEventListener('click')
-const eight = document.getElementsByClassName('8').addEventListener('click')
-const nine = document.getElementsByClassName('9').addEventListener('click')
+// Game pieces
+const one = document.getElementsByClassName('one').addEventListener('click')
+const two = document.getElementsByClassName('two').addEventListener('click')
+const three = document.getElementsByClassName('three').addEventListener('click')
+const four = document.getElementsByClassName('four').addEventListener('click')
+const five = document.getElementsByClassName('five').addEventListener('click')
+const six = document.getElementsByClassName('six').addEventListener('click')
+const seven = document.getElementsByClassName('seven').addEventListener('click')
+const eight = document.getElementsByClassName('eight').addEventListener('click')
+const nine = document.getElementsByClassName('nine').addEventListener('click')
 
 //array of the game board
 const gameBoard =
@@ -15,28 +16,37 @@ const gameBoard =
      seven,eight,nine]
 
 //solutions
-const solutions = 
-    [one,two,three]
-    [one,five,nine]
-    [one,four,seven]
-    [three,six,nine]
-    [two,five,eight]
-    [three,five,seven]
-    [four,five,six]
-    [seven,eight,nine]
+const solutions = {
+    1:[one,two,three],
+    2:[one,five,nine],
+    3:[one,four,seven],
+    4:[three,six,nine],
+    5:[two,five,eight],
+    6:[three,five,seven],
+    7:[four,five,six],
+    8:[seven,eight,nine]
+}
 
 // function for choosing one player game
 function onePlayerGame(){
     document.getElementById('player1Button').addEventListener('click', chosen)
     
+    const playerOne = [];
+    const playerTwo = [];
+
     function chosen(){
         document.getElementById('player2Button').style.display = 'none'
         alert('One player game chosen!')
     };
+
+
+
 }
+
 onePlayerGame()
 
 //function for choosing two player game
+/*
 function twoPlayerGame(){
     document.getElementById('player2Button').addEventListener('click', chosen)
     
@@ -46,3 +56,4 @@ function twoPlayerGame(){
     };
 }
 twoPlayerGame()
+*/
