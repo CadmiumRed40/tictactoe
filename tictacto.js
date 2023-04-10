665360ad705ccb3da7e592fad25993edb92974ea
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-04-10 00:17:41 -0400

    removing game modes. redundant. adding logic

--- tictacto.js
+++ tictacto.js
@@ -14,6 +14,29 @@ const gameBoard = {
 const playerOne = [];
 const playerTwo = [];
 
+for (const element of Object.values(gameBoard)) {
+    element.addEventListener('click', () => { 
+    if (playerOne.includes(element) || playerTwo.includes(element)) {
+        return;
+        }
+
+        if (playerOne.length === playerTwo.length) {
+            element.textContent = 'X'
+            playerOne.push(element);
+            console.log(`Player One: ${playerOne}`);
+        } else {
+            element.textContent = 'O';
+            playerTwo.push(element);
+            console.log(`Player Two: ${playerTwo}`);
+        }
+
+        checkForWin();
+        checkForTie();
+    });
+}
+
+
+/*
 gameBoard.forEach((element) => {
     element.addEventListener('click', () => {
         playerOne.push(element)
@@ -21,25 +44,50 @@ gameBoard.forEach((element) => {
         console.log(`Player One:${playerOne}`)
     })
 })
+*/
 
 //solutions
 const solutions = {
-    1:[one,two,three],
-    2:[one,five,nine],
-    3:[one,four,seven],
-    4:[three,six,nine],
-    5:[two,five,eight],
-    6:[three,five,seven],
-    7:[four,five,six],
-    8:[seven,eight,nine]
+    1:[gameBoard.one,gameBoard.two,gameBoard.three],
+    2:[gameBoard.one,gameBoard.five,gameBoard.nine],
+    3:[gameBoard.one,gameBoard.four,gameBoard.seven],
+    4:[gameBoard.three,gameBoard.six,gameBoard.nine],
+    5:[gameBoard.two,gameBoard.five,gameBoard.eight],
+    6:[gameBoard.three,gameBoard.five,gameBoard.seven],
+    7:[gameBoard.four,gameBoard.five,gameBoard.six],
+    8:[gameBoard.seven,gameBoard.eight,gameBoard.nine]
+};
+
+function checkForWin() {
+    for (const solution of Object.values(solutions)) {
+        const [a,b,c] = solution;
+        if(a.textContent!== '' && a.textContent ===b.textContent && b.textContent === c.textContent){
+            console.log(`Player ${playerOne.length > playerTwo.length ? 'One' : 'Two'} wins!`);
+            startOver();
+            return;
+        }
+    }
 }
+
+
 
 function startOver(){
     document.getElementById('redo').addEventListener('click', reset)
 
+    for(const element of Object.values(gameBoard)) {
+        element.textContent = ''
+    }
+
+    playerOne.length = 0;
+    playerTwo.length = 0;
+
     function reset(){
         document.getElementById('player2Button').style.display = 'block'
         document.getElementById('player1Button').style.display = 'block'
+
+        playerOne.length = 0;
+        playerTwo.length = 0;
+
     }
 }
 
